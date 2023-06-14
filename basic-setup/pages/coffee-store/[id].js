94d290a47e97834cf5a376c303855e355541b4b6
khaya05 <khayamnyandu5@gmail.com>
2023-06-14 06:31:00 +0200

    getStaticPaths

--- basic-setup/pages/coffee-store/[id].js
+++ basic-setup/pages/coffee-store/[id].js
@@ -1,13 +1,31 @@
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
+import coffeeStoresData from '../../data/coffee-stores.json';
+
+export function getStaticProps({ params }) {
+  return {
+    props: {
+      coffeeStores: coffeeStoresData.find(
+        ({ id }) => id.toString() === params.id
+      ), // dynamic id
+    },
+  };
+}
+
+export function getStaticPaths() {
+  return {
+    paths: [{ params: { id: '0' } }, { params: { id: '1' } }],
+    fallback: false,
+  };
+}
+
 const CoffeeStore = () => {
   const router = useRouter();
   const { id } = router.query;
   return (
     <>
-      <div>Coffee store Page id: {id}</div>;
-      <Link href="/">Back to Home</Link>
+      <div>Coffee store Page id: {id}</div>;<Link href="/">Back to Home</Link>
     </>
   );
 };
