a5bf36bbcb1a98dfaa589f4f9427139486480997
khaya05 <khayamnyandu5@gmail.com>
2023-06-14 16:19:27 +0200

    render stores near

--- basic-setup/lib/coffee-stores.js
+++ basic-setup/lib/coffee-stores.js
@@ -1,7 +1,7 @@
 import { createApi } from 'unsplash-js';
 
 const unsplash = createApi({
-  accessKey: process.env.UNSPLASH_ACCESS_Key,
+  accessKey: process.env.NEXT_PUBLIC_UNSPLASH_ACCESS_Key,
 });
 
 const getUrlForCoffeeStores = (latLong, query, limit) => {
@@ -11,31 +11,34 @@ const getUrlForCoffeeStores = (latLong, query, limit) => {
 const getListOfCoffeeStorePhotos = async () => {
   const photos = await unsplash.search.getPhotos({
     query: 'coffee shop',
-    page: 1,
-    perPage: 1,
+    perPage: 40,
   });
 
   const unsplashResults = photos.response.results;
   return unsplashResults.map((result) => result.urls['small']);
 };
 
-export const fetchCoffeeStores = async () => {
+export const fetchCoffeeStores = async (
+  latlng = '43.653833032607096%2C-79.37896808855945',
+  limit = 6
+) => {
+  const photos = await getListOfCoffeeStorePhotos();
+  const response = await fetch(
+    getUrlForCoffeeStores(latlng, 'coffee stores', limit)
+  );
+
   const options = {
     method: 'GET',
     headers: {
       Accept: 'application/json',
-      Authorization: process.env.FOURSQUARE_API_KEY,
+      Authorization: process.env.NEXT_PUBLIC_FOURSQUARE_API_KEY,
     },
   };
 
-  const response = await fetch(
-    getUrlForCoffeeStores(
-      '43.653833032607096%2C-79.37896808855945',
-      'coffee',
-      6
-    ),
-    options
-  );
+  // const response = await fetch(
+  //   getUrlForCoffeeStores(latlng, 'coffee', 6),
+  //   options
+  // );
   const data = await response.json();
   return data.results.map((result, idx) => {
     const neighborhood = location.neighborhood;
--- basic-setup/pages/index.js
+++ basic-setup/pages/index.js
@@ -5,6 +5,7 @@ import Banner from '../components/banner';
 import Card from '../components/card';
 import { fetchCoffeeStores } from '../lib/coffee-stores';
 import useTrackLocations from '../hooks/use-track-location';
+import { useEffect, useState } from 'react';
 
 export async function getStaticProps(context) {
   const coffeeStores = await fetchCoffeeStores();
@@ -16,9 +17,26 @@ export async function getStaticProps(context) {
   };
 }
 
+// Home
 export default function Home({ coffeeStores }) {
   const { handleTrackLocation, latlng, locationErrorMsg, isLoading } =
     useTrackLocations();
+
+  const [coffeeStoresNearMe, setCoffeeStoresNearMe] = useState('');
+  const [coffeeStoresError, setCoffeeStoresError] = useState(null);
+
+  useEffect(() => {
+    const setCoffeeStoresByLocation = async function (latlng) {
+      try {
+        const stores = await fetchCoffeeStores(latlng, 30);
+        setCoffeeStores(stores);
+      } catch (error) {
+        console.log({ error });
+        setCoffeeStoresError(error.message);
+      }
+    };
+    setCoffeeStoresByLocation();
+  }, [latlng]);
 
   const handleOnBannerBtnClick = () => {
     handleTrackLocation();
@@ -39,6 +57,31 @@ export default function Home({ coffeeStores }) {
         <div className={styles.heroImage}>
           <Image src="/static/hero-image.png" width={700} height={400} />
         </div>
+
+        {coffeeStoresNearMe.length > 0 && (
+          <>
+            {' '}
+            <div className={styles.sectionWrapper}>
+              <h2 className={styles.heading2}>Stores near me</h2>
+              <div className={styles.cardLayout}>
+                {coffeeStoresNearMe.map(({ name, imgUrl, id }) => {
+                  return (
+                    <Card
+                      key={id}
+                      name={name}
+                      to={`/coffee-store/${id}`}
+                      imgUrl={
+                        imgUrl ||
+                        'https://images.unsplash.com/photo-1504753793650-d4a2b783c15e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80'
+                      }
+                      className={styles.card}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          </>
+        )}
 
         {coffeeStores.length > 0 && (
           <>
