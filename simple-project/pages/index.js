6a5393417de1af8fe6b0aefc79e43c7ad2be6307
khaya05 <khayamnyandu5@gmail.com>
2023-06-15 09:58:13 +0200

    build project

--- simple-project/pages/index.js
+++ simple-project/pages/index.js
@@ -1,4 +1,12 @@
+import Link from 'next/link';
 
 export default function Home() {
-  return <h1>NextJs pre-rendering</h1>;
+  return (
+    <>
+      <Link href="/users">
+        <a>Users</a>
+      </Link>
+      <h1>NextJs pre-rendering</h1>;
+    </>
+  );
 }
