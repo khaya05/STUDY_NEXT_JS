89ebad15d4c5b48249d7ca904d66764bbe80e377
khaya05 <khayamnyandu5@gmail.com>
2023-06-17 08:43:13 +0200

    dynamic api routes

--- next-api/pages/api/comments/[commentId].js
+++ next-api/pages/api/comments/[commentId].js
@@ -1,19 +1,49 @@
 import { comments } from '../../../data/comments';
 
-export default function handler(req, res) {
-  const { commentId } = req.query;
+export default function Comment({ comment }) {
+  return (
+    <div>
+      {comment.id} {comment.text}
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  const paths = comments.map((comment) => {
+    return { params: { commentId: comment.id } };
+  });
+
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps(context) {
+  const { params } = context;
+  const { commentId } = params;
+
   const comment = comments.find(
     (comment) => comment.id === parseInt(commentId)
   );
-
-  switch (req.method) {
-    case 'DELETE':
-      comments.splice(commentId - 1, 1);
-      res.status(204).json(comment);
-      break;
-
-    default:
-      res.status(200).json(comment);
-      break;
-  }
+  return {
+    props: {
+      comment,
+    },
+  };
 }
+
+// export default function handler(req, res) {
+//   const { commentId } = req.query;
+//   const comment = comments.find(
+//     (comment) => comment.id === parseInt(commentId)
+//   );
+
+//   switch (req.method) {
+//     case 'DELETE':
+//       comments.splice(commentId - 1, 1);
+//       res.status(204).json(comment);
+//       break;
+
+//     default:
+//       res.status(200).json(comment);
+//       break;
+//   }
+// }
