efda9cfadccdedf50ad416474c0b8c4a6b8101a9
khaya05 <khayamnyandu5@gmail.com>
2023-06-20 12:12:45 +0200

    next intro

---
+++ next-01/app/about/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+function About() {
+  return (
+    <>
+      <h1>About</h1>
+      <Link href="/"> {'<= Home page'}</Link>
+    </>
+  );
+}
+
+export default About;
--- next-01/app/page.tsx
+++ next-01/app/page.tsx
@@ -1,5 +1,12 @@
+import Link from 'next/link';
 import styles from './page.module.css';
 
 export default function Home() {
-  return <main>Hello</main>;
+  return (
+    <main>
+      <h1>Hello</h1>
+      <Link href='/about'>{'about =>'}</Link>
+    </main>
+  );
 }
+ 
