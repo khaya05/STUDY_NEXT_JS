010624ac47f5e53a48cd12684ca40a5f399e211f
khaya05 <khayamnyandu5@gmail.com>
2023-06-20 11:36:13 +0200

    basic setup

--- next-01/app/page.tsx
+++ next-01/app/page.tsx
@@ -1,8 +1,5 @@
-import { Inter } from "next/font/google"
-const inter = Inter({subsets:['latin']})
+import styles from './page.module.css';
 
 export default function Home() {
-  return (
-   <main className={inter.className}></main>
-  )
+  return <main>Hello</main>;
 }
