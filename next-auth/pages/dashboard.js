05288e60f1505fe2cded21469349b957711d2538
khaya05 <khayamnyandu5@gmail.com>
2023-06-20 05:47:35 +0200

    session provider

--- next-auth/pages/dashboard.js
+++ next-auth/pages/dashboard.js
@@ -1,5 +1,30 @@
-import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { signIn, useSession } from 'next-auth/react';
+import { useEffect } from 'react';
 
 export default function Dashboard() {
-  return <h1>Dashboard page</h1>;
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  console.log(session, status);
+
+  useEffect(() => {
+    if (status === 'authenticated' && !session) {
+      signIn();
+    } else if (status === 'loading' && session === undefined) {
+      // Redirect the user to the sign-in page
+      router.push('/api/auth/signin');
+    }
+  }, [session, status, router]);
+
+  if (status === 'loading' && session === undefined) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (status === 'authenticated') {
+    return <h1>Dashboard page</h1>;
+  }
+
+  // This block will only be reached if the user is not signed in and the status is not 'loading'
+  return null;
 }
