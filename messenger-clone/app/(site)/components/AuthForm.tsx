d018e5177e61380c6aa8124bd382b98327d963ba
khaya05 <khayamnyandu5@gmail.com>
2023-06-26 03:13:54 +0200

    basic register

--- messenger-clone/app/(site)/components/AuthForm.tsx
+++ messenger-clone/app/(site)/components/AuthForm.tsx
@@ -6,6 +6,7 @@ import { useCallback, useState } from 'react';
 import { FieldValues, useForm, SubmitHandler } from 'react-hook-form';
 import AuthSocialButton from './AuthSocialButton';
 import { BsGithub, BsGoogle } from 'react-icons/bs';
+import axios from 'axios'
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -34,7 +35,7 @@ export default function AuthForm() {
     setIsLoading(true);
 
     if (variant === 'REGISTER') {
-      // axios register
+      axios.post('/api/register', data)
     }
 
     if (variant === 'LOGIN') {
